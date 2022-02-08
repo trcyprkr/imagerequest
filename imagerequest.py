253d9553b3a097e9817dfea6d3f7be3e98b2fce2
TrcyPrkr <trcyprkr@users.noreply.github.com>
2022-02-07 21:09:22 -0600

    Add files via upload

--- imagerequest.py
+++ imagerequest.py
@@ -1,27 +1,45 @@
 import urllib.request
 import time
-import datetime as dt
+from datetime import datetime
+import requests
 
-def get_sleeptime(minute, second):
-    now = dt.datetime.now()
-    sched = now.replace(minute=minute, second=second)
-    sleeptime = (sched - now).total_seconds()
-    if sleeptime < 0:
-        sleeptime += 3600
-    return sleeptime
 
-apgar = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\apgarvillage.jpg'
 apgarURL = 'https://www.nps.gov/webcams-glac/ApgarVillage.jpg'
-
-missoula = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\missoula.jpg'
 missoulaURL = 'https://wingsvirtualtours.com/webcams/missoula_valley/public_html/cam/streaming/mp/current.jpg'
+aspenURL = 'https://coloradowebcam.net/webcam/aspenmtn/current.jpg'
+boulderURL = 'https://coloradowebcam.net/webcam/boulder/current.jpg'
+soprisURL = 'https://coloradowebcam.net/webcam/sopris/current.jpg'
+#**************************************************************************
+apgar = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\apgar.jpg'
+missoula = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\missoula.jpg'
+aspen = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\aspen.jpg'
+boulder = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\boulder.jpg'
+sopris = r'\Users\trcyp\Downloads\PiClock-Python3-SerBrynden\PiClock-Python3-SerBrynden\Clock\images\slideshow\sopris.jpg'
+#***************************************************************************
+print(time.asctime())
 
 while True:
-    print("Waiting...")
-    time.sleep(get_sleeptime(0, 0))
-    print("Requesting")
-    urllib.request.urlretrieve(apgarURL, apgar)
-    time.sleep(10)
-    urllib.request.urlretrieve(missoulaURL, missoula)
-    time.sleep(5)
-    print("Done")
+    r = time.localtime()
+    h = r.tm_hour
+    m = r.tm_min
+    s = r.tm_sec
+    if h >= 7 and h <= 19:
+        #time.sleep(1800)
+        print("Requesting")
+        apgarData = requests.get(apgarURL).content
+        with open(apgar, 'wb') as handler:
+            handler.write(apgarData)
+        missoulaData = requests.get(missoulaURL).content
+        with open(missoula, 'wb') as handler:
+            handler.write(missoulaData)
+        aspenData = requests.get(aspenURL).content
+        with open(aspen, 'wb') as handler:
+            handler.write(aspenData)
+        boulderData = requests.get(boulderURL).content
+        with open(boulder, 'wb') as handler:
+            handler.write(boulderData)
+        soprisData = requests.get(soprisURL).content
+        with open(sopris, 'wb') as handler:
+            handler.write(soprisData)
+            
+        print("Done")
